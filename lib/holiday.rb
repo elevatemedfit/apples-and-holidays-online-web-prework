6e01a86e30c33a137f560809a3146ed5c5fdd6a7
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-08 23:39:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
 end
 
-add_supply_to_memorial_day(holiday_hash, 'grill')
 def add_supply_to_memorial_day(holiday_hash, supply)
 holiday_hash[:spring][:memorial_day]<<supply
 holiday_hash
